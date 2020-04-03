149516bf8eca5b10350553192baa77b2251b7f30
Jason Stedman <namdets@gmail.com>
2020-04-03 10:30:39 -0400

    jenkins container management scripts

---
+++ start-jenkins.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo docker run -d \
+-v /var/jenkins_home/jobs:/var/jenkins_home/jobs \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-p 8080:8080 --name jenkins --restart always pic-sure-jenkins:LATEST

487149f95048a0dc6560a29b280cdbaf0d0444f4
Jason Stedman <namdets@gmail.com>
2020-04-03 11:32:48 -0400

    remove container when stopped

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo docker run -d \
+sudo docker run -d -rm \
 -v /var/jenkins_home/jobs:/var/jenkins_home/jobs \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -p 8080:8080 --name jenkins --restart always pic-sure-jenkins:LATEST

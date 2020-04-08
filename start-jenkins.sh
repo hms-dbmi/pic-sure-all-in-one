604ada5f110d8a7e7e89780df87f1913e3105997
Jason Stedman <namdets@gmail.com>
2020-04-08 15:13:56 -0400

    configuration updates, also exposing mysql to jenkins so it can update database configuration

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -2,4 +2,6 @@
 sudo docker run -d \
 -v /var/jenkins_home/jobs:/var/jenkins_home/jobs \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock \
+-v /root/.my.cnf:/root/.my.cnf \
 -p 8080:8080 --name jenkins --restart always pic-sure-jenkins:LATEST

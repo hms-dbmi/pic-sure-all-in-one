54ec75169912c19f20c2fc8ab863ac46e4c8f3aa
Jason Stedman <namdets@gmail.com>
2020-04-09 11:23:08 -0400

    fixing view and adding /usr/local/docker-config to jenkins container so it can configure stuffg

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -d \
 -v /var/jenkins_home/jobs:/var/jenkins_home/jobs \
+-v /usr/local/docker-config:/usr/local/docker-config \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock \
 -v /root/.my.cnf:/root/.my.cnf \
 -p 8080:8080 --name jenkins --restart always pic-sure-jenkins:LATEST

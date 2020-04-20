6d39c39a382be5f077401ec0cc5f404082d11bdb
Jason Stedman <namdets@gmail.com>
2020-04-20 16:29:31 -0400

    copying in and setting vars in configs

--- update-jenkins.sh
+++ update-jenkins.sh
@@ -3,9 +3,8 @@
 mkdir -p /var/jenkins_home_bak
 cp -r /var/jenkins_home/* /var/jenkins_home_bak/
 rm -rf /var/jenkins_home/*
-cp -r initial-configuration/jenkins/jenkins-docker/* /var/jenkins_home/
-docker build -t pic-sure-jenkins:`git log -n 1 | grep commit | cut -d ' ' -f 2 | cut -c 1-7` initial-configuration/jenkins/jenkins-docker
-docker tag pic-sure-jenkins:`git log -n 1 | grep commit | cut -d ' ' -f 2 | cut -c 1-7` pic-sure-jenkins:LATEST
-sed -i "s|`cat /var/jenkins_home_bak/config.xml | grep -A1 project_specific_override_repo | tail -1 | sed 's/<\/*string>//g'`|$env.PROJECT_SPECIFIC_OVERRIDE_REPOSITORY|g" /var/jenkins_home/config.xml
-sed -i "s|`cat /var/jenkins_home_bak/config.xml | grep -A1 release_control_repo | tail -1 | sed 's/<\/*string>//g'`|$env.RELEASE_CONTROL_REPOSITORY|g" /var/jenkins_home/config.xml
+cp -r initial-configuration/jenkins/jenkins-docker/config.xml /var/jenkins_home/
+cp -r initial-configuration/jenkins/jenkins-docker/scriptApproval.xml /var/jenkins_home/
+sed -i "s|__PROJECT_SPECIFIC_OVERRIDE_REPO__|`cat /var/jenkins_home_bak/config.xml | grep -A1 project_specific_override_repo | tail -1 | sed 's/<\/*string>//g'`|g" /var/jenkins_home/config.xml
+sed -i "s|__RELEASE_CONTROL_REPO__|`cat /var/jenkins_home_bak/config.xml | grep -A1 release_control_repo | tail -1 | sed 's/<\/*string>//g'`|g" /var/jenkins_home/config.xml
 ./start-jenkins.sh

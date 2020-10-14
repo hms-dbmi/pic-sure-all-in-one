d9ea0649a5da5b6d09ca7c8da1ddf3776c66984f
Jeremy Nix <Jeremy.Nix@cchmc.org>
2020-10-14 09:21:51 -0400

    remove setProxy virtual

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -5,14 +5,13 @@ if [ -f /usr/local/docker-config/setProxy.sh ]; then
 fi
 
 docker run -d \
--e http_proxy=$http_proxy \
--e https_proxy=$https_proxy \
--e no_proxy=$no_proxy \
--v /var/jenkins_home:/var/jenkins_home \
--v /usr/local/docker-config:/usr/local/docker-config \
--v /var/run/docker.sock:/var/run/docker.sock \
--v /root/.my.cnf:/root/.my.cnf \
--v /root/.m2:/root/.m2 \
--v /root/setProxy.sh:/root/setProxy.sh \
--p 8080:8080 --name jenkins pic-sure-jenkins:LATEST
+  -e http_proxy=$http_proxy \
+  -e https_proxy=$https_proxy \
+  -e no_proxy=$no_proxy \
+  -v /var/jenkins_home:/var/jenkins_home \
+  -v /usr/local/docker-config:/usr/local/docker-config \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /root/.my.cnf:/root/.my.cnf \
+  -v /root/.m2:/root/.m2 \
+  -p 8080:8080 --name jenkins pic-sure-jenkins:LATEST
 docker restart jenkins

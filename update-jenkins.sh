8d3b5c93d434a162bff2bcbb62aeb94d4aa400f2
Jeremy Nix <Jeremy.Nix@cchmc.org>
2020-10-15 04:58:02 -0400

    update for retaining proxy configs

--- update-jenkins.sh
+++ update-jenkins.sh
@@ -14,7 +14,6 @@ sed -i "s|__PROJECT_SPECIFIC_OVERRIDE_REPO__|`cat /var/jenkins_home_bak/config.x
 sed -i "s|__RELEASE_CONTROL_REPO__|`cat /var/jenkins_home_bak/config.xml | grep -A1 release_control_repo | tail -1 | sed 's/<\/*string>//g' | sed 's/ //g' `|g" /var/jenkins_home/config.xml
 
 # Pull through previous proxy configurations
-cp -r /var/jenkins_home_bak/.m2 /var/jenkins_home/
-cp -r /var/jenkins_home_bak/proxy.xml /var/jenkins_home/
+cp /var/jenkins_home_bak/setProxy.sh /var/jenkins_home/setProxy.sh
 
 ./start-jenkins.sh

6f2a66104256a67aa45a09ae578ffa5d934f8e5f
Jeremy Nix <Jeremy.Nix@cchmc.org>
2020-10-15 06:05:27 -0400

    stop docker before removing; log dir cleanup

--- initial-configuration/uninstall.sh
+++ initial-configuration/uninstall.sh
@@ -3,21 +3,23 @@
 echo "Checking docker status..."
 systemctl is-active --quiet docker
 if [ $? != "0" ]; then
-        echo "Starting docker..."
-        sudo systemctl start docker
+	echo "Starting docker..."
+	systemctl start docker
 fi
 
 # Prune docker images
 containers=$(docker ps -a -q)
 if [ ! -z "$containers" ]; then
-        docker stop $(docker ps -a -q)
-        docker rm $(docker ps -a -q)
+	docker stop $(docker ps -a -q)
+	docker rm $(docker ps -a -q)
 fi
 images=$(docker ps -a -q)
 if [ ! -z "$images" ]; then
-        docker rmi $(docker images -a -q)
+	docker rmi $(docker images -a -q)
 fi
 docker system prune -a
+
+systemctl stop docker
 
 rm -rf /usr/local/docker-config
 
@@ -37,5 +39,8 @@ rm -f ~/.my.cnf
 rm -rf /var/lib/mysql
 
 rm -rf /var/jenkins_home
+rm -rf /var/jenkins_home_bak
 rm -rf /var/log/httpd-docker-logs
 rm -rf /var/log/jenkins-docker-logs
+rm -rf /var/log/wildfly-docker-logs
+rm -rf /var/log/wildfly-docker-os-logs

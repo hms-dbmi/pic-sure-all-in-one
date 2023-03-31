a845de0f362bb7cf23dcd379e66dbb920ddf2e3c
Samantha <samantha.piatt@childrens.harvard.edu>
2023-03-31 08:57:47 -0400

    Add debug logging

--- initial-configuration/redhat-install-dependencies.sh
+++ initial-configuration/redhat-install-dependencies.sh
@@ -54,9 +54,9 @@ docker network inspect hpdsNet --format "{{.Name}}: {{.Id}}" 2>&1  ||  docker ne
 
 sleep 3s
 
-docker run -it --rm hello-world
-docker run -it --rm  --name test1 --network=picsure hello-world
-docker run -it --rm  --name test2 --network=hpdsNet hello-world && docker rmi hello-world
+docker run -it --rm --log-level=debug hello-world
+docker run -it --rm --log-level=debug --name test1 --network=picsure hello-world
+docker run -it --rm --log-level=debug --name test2 --network=hpdsNet hello-world && docker rmi hello-world
 firewall-cmd --add-port=8080/tcp
 firewall-cmd --runtime-to-permanent
 podman network reload --all

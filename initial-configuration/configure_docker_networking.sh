33c784c00566a2f6906b743936959d930cca63b1
Luke Sikina <lucas.skina@gmail.com>
2023-11-22 15:50:17 -0500

    [ALS-5080] Dockerize MySQL
    - Create new install script
      - Install docker
      - Do better checks to see if docker is working
      - Create picsure database via docker compose
    - change db interactions in Jenkins
      - Rather than doing a docker run, exec into the running mysql
    container
      - Use the auth provided by the env vars
    - Add mysql env vars to jenkins container

--- initial-configuration/configure_docker_networking.sh
+++ initial-configuration/configure_docker_networking.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ifacename=`docker network ls | awk '/picsure/ {print "br-"$1}'`
 sysctl -w net.ipv4.conf.$ifacename.route_localnet=1
-iptables -t nat -I PREROUTING -i $ifacename -d 172.18.0.1 -p tcp --dport 3306 -j DNAT --to 127.0.0.1:3306
+iptables -t nat -I PREROUTING -i $ifacename -d picsure-db -p tcp --dport 3306 -j DNAT --to 127.0.0.1:3306
 iptables -t filter -I INPUT -i $ifacename -d 127.0.0.1 -p tcp --dport 3306 -j ACCEPT

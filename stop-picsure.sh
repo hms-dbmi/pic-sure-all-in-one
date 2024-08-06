8eb1ff0dd6fecf85d792e314656b8ccf69effde8
Luke Sikina <lucas.skina@gmail.com>
2024-08-06 10:54:16 -0400

    [ALS-6725] Dictionary Start / Stop + .env
    - start changes
    - stop changes
    - upload env file

--- stop-picsure.sh
+++ stop-picsure.sh
@@ -3,3 +3,7 @@ docker stop hpds && docker rm hpds
 docker stop httpd && docker rm httpd
 docker stop wildfly && docker rm wildfly
 docker stop psama && docker rm psama
+
+if test -d $DOCKER_CONFIG_DIR/dictionary then
+  docker compose -f $DOCKER_CONFIG_DIR/dictionary/docker-compose.yml --env-file $DOCKER_CONFIG_DIR/dictionary/.env down
+fi

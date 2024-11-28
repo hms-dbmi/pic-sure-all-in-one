60c07a691f566d80c97cd902a56d18cfb424d471
Luke Sikina <lucas.skina@gmail.com>
2024-11-28 12:51:25 -0500

    [ALS-7887] Start script improvements
    - Fixed it not working inside jenkins
    - Refined the coupling between jenkins and bare metal script
    - Make script exit with bad code if run commands fail

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -6,6 +6,7 @@ if [ -f $DOCKER_CONFIG_DIR/setProxy.sh ]; then
    . $DOCKER_CONFIG_DIR/setProxy.sh
 fi
 
+docker stop jenkins && docker rm jenkins
 docker run -d \
   -e http_proxy="$http_proxy" \
   -e https_proxy="$https_proxy" \
@@ -14,6 +15,8 @@ docker run -d \
   -v /var/jenkins_cert:/var/jenkins_cert \
   -v "$DOCKER_CONFIG_DIR"/hpds_csv/:/usr/local/docker-config/hpds_csv/ \
   -v /var/jenkins_home:/var/jenkins_home \
+  -v ./start-picsure.sh:/scripts/start-picsure.sh \
+  -v ./stop-picsure.sh:/scripts/stop-picsure.sh \
   -v "$DOCKER_CONFIG_DIR":/usr/local/docker-config \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$MYSQL_CONFIG_DIR"/.my.cnf:/root/.my.cnf \
@@ -21,13 +24,3 @@ docker run -d \
   -v /etc/hosts:/etc/hosts \
   -v /usr/local/pic-sure-services:/pic-sure-services \
   -p 8080:8080 --name jenkins pic-sure-jenkins:LATEST
-
-# These would normally be volume mounts, but mounting volumes in volumes is bad vibes
-# and it was breaking the backup logic in update-jenkins
-docker exec jenkins mkdir -p /var/jenkins_home/workspace/Start\ PIC-SURE
-docker exec jenkins mkdir -p /var/jenkins_home/workspace/Stop\ PIC-SURE
-# Docker cp doesn't add parent dirs. Not even an option. Thanks, I guess?
-docker cp start-picsure.sh jenkins:/var/jenkins_home/workspace/Start\ PIC-SURE/start-picsure.sh
-docker cp stop-picsure.sh jenkins:/var/jenkins_home/workspace/Stop\ PIC-SURE/stop-picsure.sh
-
-docker restart jenkins

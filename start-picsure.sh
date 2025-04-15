a54c3b99a4eea208772b5170f61355e0d43fcfca
Luke Sikina <lucas.sikina@gmail.com>
2025-04-15 15:39:11 -0400

    Update start-picsure.sh

--- start-picsure.sh
+++ start-picsure.sh
@@ -91,7 +91,6 @@ docker run --name=wildfly --restart always --network=picsure --network=hpds --ne
   $EMAIL_TEMPLATE_VOLUME \
   -v $DOCKER_CONFIG_DIR/wildfly/wildfly_mysql_module.xml:/opt/jboss/wildfly/modules/system/layers/base/com/sql/mysql/main/module.xml  \
   -v $DOCKER_CONFIG_DIR/wildfly/mysql-connector-java-5.1.49.jar:/opt/jboss/wildfly/modules/system/layers/base/com/sql/mysql/main/mysql-connector-java-5.1.49.jar  \
-  -p 5007:5007 \
   --env-file $CURRENT_FS_DOCKER_CONFIG_DIR/wildfly/wildfly.env \
   -d hms-dbmi/pic-sure-wildfly:LATEST \
   || exit 2

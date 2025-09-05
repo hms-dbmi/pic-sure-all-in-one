74a24710fb7f217386d0685c69e1233e7f65713c
Samantha <samantha.piatt@childrens.harvard.edu>
2025-09-05 14:58:38 -0400

    Add variables to stop script so it can find the config files for optional services. (#206)

--- stop-picsure.sh
+++ stop-picsure.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# A note to developers: if you use /usr/local/docker-config to refer to a place on the host file system
+# 99 times out of 100 you are WRONG and you have just made a bug. Please:
+# - Consider using $DOCKER_CONFIG_DIR instead
+# - Challenge your own understanding of where files are located in docker and on the host file system and
+# how that does or doesn't change the commands you run when inside Jenkins
+
+DOCKER_CONFIG_DIR="${DOCKER_CONFIG_DIR:-/usr/local/docker-config}"
+# Use this for file system checks. Use DOCKER_CONFIG_DIR for docker commands.
+# Except for --env_file commands, which refer to the current file system, not the root fs
+CURRENT_FS_DOCKER_CONFIG_DIR="${CURRENT_FS_DOCKER_CONFIG_DIR:-$DOCKER_CONFIG_DIR}"
+
+if [ -f "$CURRENT_FS_DOCKER_CONFIG_DIR/setProxy.sh" ]; then
+   . $CURRENT_FS_DOCKER_CONFIG_DIR/setProxy.sh
+fi
+
 # Optional services
 [[ -d "$CURRENT_FS_DOCKER_CONFIG_DIR/hpds" ]] && INCLUDE_HPDS=true || INCLUDE_HPDS=false
 echo "INCLUDE_HPDS=$INCLUDE_HPDS"

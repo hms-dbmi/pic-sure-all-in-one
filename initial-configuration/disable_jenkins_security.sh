9d5a8ad32186ef41b584bdd606a685466be75428
James <Jamestp19@gmail.com>
2026-06-09 11:15:19 -0400

    Remove the legacy initial-configuration/ tree
    With service configuration now under config/ and demo data under
    demo-data/, the entire initial-configuration/ directory is superseded:
    legacy Jenkins job XML, JupyterHub and MySQL Docker assets, and the old
    install / migrate / convert shell scripts are all deleted.

--- initial-configuration/disable_jenkins_security.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# If you would like to disable Jenkins login you can run this script.
-# chmod +x disable_jenkins_security.sh
-# ./disable_jenkins_security.sh
-
-docker exec jenkins bash -c "sed -i 's/<useSecurity>true<\/useSecurity>/<useSecurity>false<\/useSecurity>/g' /var/jenkins_home/config.xml"
-docker restart jenkins
